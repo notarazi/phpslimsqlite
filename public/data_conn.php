caad08bd90ae1aa80d440c426b7955b78d05fd09
notarazi <notarazi@gmail.com>
2020-10-05 13:54:46 +0800

    Update data_conn.php

--- public/data_conn.php
+++ public/data_conn.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 
-$connection = new PDO("sqlite:../data/mydb.sdb");
+$connection = new PDO("sqlite:./data/mydb.sdb");
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
 
